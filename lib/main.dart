6e5b271e2b3ffdaa2e971f1c8893a049cfffed38
likhithaap <likhiparuchuri132@gmail.com>
2021-10-21 13:05:56 +0530

    created a new class for status page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/status_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -189,57 +190,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   ],
                 ))
               ]),
-              Column(children: [
-                Expanded(
-                    child: ListView(
-                  children: <Widget>[
-                    ListTile(
-                        title: Text(
-                          "My status",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text("Tap to add status update"),
-                        leading: Stack(
-                          children: [
-                            CircleAvatar(
-                              radius: 24.0,
-                              child: ClipRRect(
-                                child:
-                                    Image.network('https://i.pravatar.cc/200'),
-                                borderRadius: BorderRadius.circular(50.0),
-                              ),
-                            ),
-                            Positioned(
-                                bottom: 0,
-                                right: 0,
-                                child: CircleAvatar(
-                                    radius: 11,
-                                    backgroundColor: Colors.white,
-                                    child: CircleAvatar(
-                                      radius: 9,
-                                      backgroundColor: Color(0xff085E55),
-                                      child: Icon(
-                                        Icons.add,
-                                        size: 17,
-                                        color: Colors.white,
-                                      ),
-                                    )))
-                          ],
-                        )),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      "    Recent updates",
-                      style: TextStyle(
-                        color: Color(0xff808080),
-                        fontWeight: FontWeight.w500,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ],
-                ))
-              ]),
+              
+                new status_page(),
+              
               Column(children: [
                 Expanded(
                     child: ListView(
